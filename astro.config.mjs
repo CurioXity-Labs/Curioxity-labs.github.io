35bcb7cb07986efbc312f35770fe1374e1b093be
Nikhil <118874572+Nikhil690@users.noreply.github.com>
2025-07-19 00:22:53 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { siteConfig } from './src/data/site.config'
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	base: '/CurioXityLabs.github.io/',
+	base: '/CurioXity-Labs.github.io/',
 	site: siteConfig.site,
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
