a4521ee2ebbbe629d5b55a2dddf38bbe0acf437e
Nikhil690 <nikhiljangid30@outlook.com>
2025-07-18 16:53:26 +0000

    Add base path configuration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import { siteConfig } from './src/data/site.config'
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
+	base: './',
 	site: siteConfig.site,
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
